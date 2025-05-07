cf2c44887c1719616fe54bb5a9ea420465f08e23
Richard McGinn <richard.mcginn@blakyaks.com>
2025-05-07 16:31:59 +0100

    Terraform format

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "core-network-rg" {
-  name = "rg-uks-net-01"
+  name     = "rg-uks-net-01"
   location = "uksouth"
 }
 
 resource "azurerm_virtual_network" "core-vnet" {
-  name = "vnet-uks-core-01"
-  location = azurerm_resource_group.core-network-rg.location
+  name                = "vnet-uks-core-01"
+  location            = azurerm_resource_group.core-network-rg.location
   resource_group_name = azurerm_resource_group.core-network-rg.name
-  address_space = [ "10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "core-subnet" {
-  name = "snet-uks-core-01"
+  name                 = "snet-uks-core-01"
   virtual_network_name = azurerm_virtual_network.core-vnet.name
-  resource_group_name = azurerm_resource_group.core-network-rg.name
-  address_prefixes = ["10.0.1.0/24"]
+  resource_group_name  = azurerm_resource_group.core-network-rg.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
