ee9d9f5b54f4f9e9fa80b95bd7a030c401e2bc0f
Richard McGinn <richard.mcginn@blakyaks.com>
2025-05-08 12:21:33 +0100

    Add kube_config output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,5 @@
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  value       = azurerm_kubernetes_cluster.k8s.kube_config_raw
   description = "kube_config file"
+  sensitive   = true
 }
