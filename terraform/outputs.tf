851b4005da77b630f9037981a68636546f3a3810
Richard McGinn <richard.mcginn@blakyaks.com>
2025-05-08 12:36:24 +0100

    Fix kube_config output format

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "kube_config" {
-  value       = azurerm_kubernetes_cluster.k8s.kube_config_raw
+  value       = yamlencode(azurerm_kubernetes_cluster.k8s.kube_config_raw)
   description = "kube_config file"
   sensitive   = true
 }
