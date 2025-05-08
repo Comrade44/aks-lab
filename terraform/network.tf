47f8640da0f0371e6db162019134db53be41f8e4
Richard McGinn <richard.mcginn@blakyaks.com>
2025-05-08 17:26:03 +0100

    Add db subnet

--- terraform/network.tf
+++ terraform/network.tf
@@ -16,3 +16,10 @@ resource "azurerm_subnet" "core-subnet" {
   resource_group_name  = azurerm_resource_group.core-network-rg.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_subnet" "db-subnet" {
+  name                 = "snet-uks-db-01"
+  virtual_network_name = azurerm_virtual_network.core-vnet.name
+  resource_group_name  = azurerm_resource_group.core-network-rg.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
